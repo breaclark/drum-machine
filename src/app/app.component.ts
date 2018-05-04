467ffa5e0837f6b3e15f32638ed52746e3e4590c
breab <brea.borlas@gmail.com>
2018-05-04 08:30:57 -0700

    Initiate sound array called beats in app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Sound } from './models/sounds.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Drum Machine';
+  beat : Sound[] = [new Sound("kick", "fakekickfilepath"), new Sound("snare","fakesnarefilepath"), new Sound("high-hat","fakehigh-hatfilepath")];
 }
