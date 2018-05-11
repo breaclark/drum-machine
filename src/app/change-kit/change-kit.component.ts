bd36b0f42147859d0bcbc309fd393f7a0f9d3f9b
breab <brea.borlas@gmail.com>
2018-05-11 14:57:37 -0700

    Prepare files for multiple drum-kits

--- src/app/change-kit/change-kit.component.ts
+++ src/app/change-kit/change-kit.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-change-kit',
   templateUrl: './change-kit.component.html',
   styleUrls: ['./change-kit.component.css']
 })
-export class ChangeKitComponent implements OnInit {
-
+export class ChangeKitComponent  {
+  @Output() kitSender = new EventEmitter();
   constructor() { }
 
-  ngOnInit() {
+  setLoFi() {
+    let loFiKit = [];
+    this.kitSender.emit(loFiKit);
   }
+
+  set808() {
+    let eight08Kit = [];
+    this.kitSender.emit(eight08Kit);
+  }
+
+  setBeatBox() {
+    let beatBoxKit = [];
+    this.kitSender.emit(beatBoxKit);
+  }
+
 
 }
