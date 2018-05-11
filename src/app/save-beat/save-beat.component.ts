e820a60158352250015c2a0c9a905a96bb5693c3
breab <brea.borlas@gmail.com>
2018-05-10 21:50:22 -0700

    Add sound import

--- src/app/save-beat/save-beat.component.ts
+++ src/app/save-beat/save-beat.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { BeatService } from '../beat.service';
 import { FirebaseListObservable } from 'angularfire2/database';
 import { Beat } from './../models/beat.model';
+import { Sound } from './../models/sounds.model';
 
 @Component({
   selector: 'app-save-beat',
