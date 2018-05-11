73287906e1d4b58d2d80eb85ae0d03e80108e631
breab <brea.borlas@gmail.com>
2018-05-10 20:59:00 -0700

    Save beat with generic name

--- src/app/save-beat/save-beat.component.ts
+++ src/app/save-beat/save-beat.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { BeatService } from '../beat.service';
 import { FirebaseListObservable } from 'angularfire2/database';
+import { Beat } from './../models/beat.model';
 
 @Component({
   selector: 'app-save-beat',
@@ -8,15 +9,17 @@ import { FirebaseListObservable } from 'angularfire2/database';
   styleUrls: ['./save-beat.component.css'],
   providers: [BeatService]
 })
-export class SaveBeatComponent implements OnInit {
 
+export class SaveBeatComponent implements OnInit {
+  @Input() childBeats: Sound[][];
   constructor(private beatService: BeatService) { }
 
   ngOnInit() {
   }
 
   saveBeat() {
-    console.log("hi");
+    let newBeat = new Beat("genericName", this.childBeats);
+    this.beatService.addBeat(newBeat);
   }
 
 }
